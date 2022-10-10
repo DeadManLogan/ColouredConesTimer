b33fbf1321d9812bd7d5acc7a211c5cd3fbe3b63
DeadManLogan <grigoroudisdimosthenis@gmail.com>
2022-10-10 23:06:55 +0300

    Created stop button

--- coloured-cones-timer/src/Components/StartButton.js
+++ coloured-cones-timer/src/Components/StartButton.js
@@ -1,45 +1,66 @@
 import { Button } from '@mui/material';
 import { useEffect, useState } from 'react';
 
-// TO DO: make stop button
+// DONE: make stop button
 // TO DO: ask the user about interval
 // TO DO: ask the user about cones
+// TO DO: text-to-speech
+// TO DO: create cool README
+
 
 const StartButton = () => {
-    const [numberToGo, setNumberToGo] = useState();
+    const [numberToGo, setNumberToGo] = useState(0);
+    const [isGenerating, setIsGeneratining] = useState(false);
 
-    const getRandomNumber = () => {
-        console.log('initialize')
-        let min = 1;
-        let max = 4;
-        let randomNumber = 0;
-        let array = [1, 2, 3, 4];
-        let swapV = [1, 2, 3, 4];
-        setInterval(() => {
-            while(!swapV.includes(randomNumber)) {
-                randomNumber = Math.round((Math.random() * (max-min)) + min);
-            }
-            swapV = array.filter(val => {
-                return(val!==randomNumber)
-            });
-            console.log(randomNumber)
-            // let test = randomNumber;
-            // while(randomNumber === test) {
-            //     randomNumber = Math.round((Math.random() * (max-min)) + min);
-            // }
-            setNumberToGo(randomNumber);
-        }, 1000);
-    };
+    useEffect(() => {
+        let timer = null;
 
-    const stopGettingNumbers = () => {
-        clearInterval();
-    };
+        if(isGenerating) {
+            let min = 1;
+            let max = 4;
+            let randomNumber = 0;
+            let array =  [1, 2, 3, 4];
+            let swapV = [1, 2, 3, 4];
+            timer = setInterval(() => {
+                while(!swapV.includes(randomNumber)) {
+                    randomNumber = Math.round((Math.random() * (max-min)) + min);
+                }
+                swapV = array.filter(val => {
+                    return(val!==randomNumber)
+                });
+                setNumberToGo(randomNumber);
+            }, 1000);
+        } else {
+            setNumberToGo(0);
+            clearInterval(timer);
+        }
+
+        return () => clearInterval(timer);
+    }, [isGenerating]);
+
+    // function getRandomNumber() {
+    //     let min = 1;
+    //     let max = 4;
+    //     let randomNumber = 0;
+    //     let array =  [1, 2, 3, 4];
+    //     let swapV = [1, 2, 3, 4];
+    //     timer = setInterval(() => {
+    //         while(!swapV.includes(randomNumber)) {
+    //             randomNumber = Math.round((Math.random() * (max-min)) + min);
+    //         }
+    //         swapV = array.filter(val => {
+    //             return(val!==randomNumber)
+    //         });
+    //         setNumberToGo(randomNumber);
+    //     }, 1000);
+    //     return () => clearInterval(timer);
+    // };
 
     return(
         <>
             <p>{numberToGo}</p>
-            <Button variant="contained" onClick={getRandomNumber}>Start</Button>
-            <Button variant="contained" onClick={stopGettingNumbers}>Stop</Button>
+            <Button variant="contained" onClick={() => setIsGeneratining(true)}>Start</Button>
+            <Button variant="contained" onClick={() => setIsGeneratining(false)}>Stop</Button>
         </>
     );
 };
